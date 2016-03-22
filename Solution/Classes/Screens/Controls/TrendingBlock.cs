3a6d51eaaab426267038fbe7c552c994ad1642db
Jonathan Bursztyn <jobur93@gmail.com>
2016-03-21 23:56:35 -0300

    Added LikeComponent to TrendingBlock

--- Solution/Classes/Screens/Controls/TrendingBlock.cs
+++ Solution/Classes/Screens/Controls/TrendingBlock.cs
@@ -59,7 +59,64 @@ namespace Board
 			trendingLabel.SizeToFit ();
 
 			AddSubview (trendingLabel);
+
+			CreateLikeComponent ();
+
+			AddSubview (LikeComponent);
 		}
+
+		protected UIImageView LikeComponent;
+		private UIImageView likeView;
+		private UILabel likeLabel;
+		private const int iconSize = 40;
+
+		private void CreateLikeComponent()
+		{
+			LikeComponent = new UIImageView (new CGRect (Frame.Width - 80,
+				Frame.Height - 40, 80, 40));
+
+			likeView = CreateLike (LikeComponent.Frame);
+			likeLabel = CreateLikeLabel (likeView.Center);
+
+			LikeComponent.AddSubviews (likeView, likeLabel);
+		}
+
+
+		protected UIImageView CreateLike(CGRect frame)
+		{
+			UIImageView likeView = new UIImageView(new CGRect(0, 0, iconSize, iconSize));
+
+			using (UIImage img = UIImage.FromFile ("./boardinterface/widget/like.png")) {
+				likeView.Image = img;
+				likeView.Image = likeView.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+			}
+
+			likeView.TintColor = UIColor.White;
+			likeView.Center = new CGPoint (frame.Width - 5 - iconSize / 2, frame.Height / 2);
+			return likeView;
+		}
+
+		protected UILabel CreateLikeLabel(CGPoint center)
+		{
+			int fontSize = 18;
+
+			UIFont likeFont = UIFont.SystemFontOfSize (fontSize);
+			Random rand = new Random ();
+			int randomLike = rand.Next (16, 98);
+
+			string likeText = randomLike.ToString();
+			CGSize likeLabelSize = likeText.StringSize (likeFont);
+
+			UILabel likeLabel = new UILabel(new CGRect(0, 0, likeLabelSize.Width + 20, likeLabelSize.Height));
+			likeLabel.TextColor = UIColor.White;
+			likeLabel.Font = likeFont;
+			likeLabel.Text = likeText;
+			likeLabel.Center = new CGPoint (center.X - likeLabel.Frame.Width / 2 - 10, center.Y);
+			likeLabel.TextAlignment = UITextAlignment.Center;
+
+			return likeLabel;
+		}
+
 
 	}
 }
